700219e2dc75bd0a4e770f5f10715e470630b9fa
kz <kz@kzs-MacBook-Pro.local>
2024-03-21 00:20:18 -0700

    First commit.

---
+++ main.py
@@ -0,0 +1,6 @@
+"""Main file."""
+from modules.module1 import f1
+
+if __name__ == '__main__':
+    print('Hello World!!!')
+    print(f1(1.5, 2.5))
---
+++ modules/module1.py
@@ -0,0 +1,16 @@
+"""Module 1."""
+
+from numbers import Number
+
+
+def f1(a: Number, b: Number) -> Number:
+    """Add a and b.
+
+    Args:
+        a (Number): Some number.
+        b (Number): Some number.
+
+    Returns:
+        Number: Resulting sum.
+    """
+    return a+b
