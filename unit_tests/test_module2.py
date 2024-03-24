8e4780c0848db52de43ab28e80b3f4b0154e3a39
kz <kz@kzs-MacBook-Pro.local>
2024-03-23 23:53:49 -0700

    Use a pytest fixture.

--- unit_tests/test_module2.py
+++ unit_tests/test_module2.py
@@ -3,9 +3,9 @@ import pytest
 from modules.module2 import MyClass
 
 
-def test_exception():
+def test_exception(my_fix):
     """_summary_
     """
-    cc = MyClass('Jimmy')
-    with pytest.raises(ValueError):
-        cc.throw_exception()
+    with pytest.raises(ValueError) as ex:
+        my_fix.throw_exception()
+    assert str(ex.value) == 'Jimmy loves value errors!'
