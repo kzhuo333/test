2839d27ff49210637ed2e47ef483dac078ad9d1c
kz <kz@kzs-MacBook-Pro.local>
2024-03-23 22:59:39 -0700

    Add pytest.

---
+++ unit_tests/test_module1.py
@@ -0,0 +1,12 @@
+"""Docstring."""
+import modules.module1 as m1
+
+
+def test_f1():
+    """Docstring."""
+    assert m1.f1(2, 2) == 4
+
+
+def test_f2():
+    """Docstring."""
+    assert m1.f2(2, 2) == 4
