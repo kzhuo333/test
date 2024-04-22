0cc9d972c47681f4725ae72ba9ae3efbf279f244
kz <kz@kzs-MacBook-Pro.local>
2024-04-21 17:37:44 -0700

    Add third module.

---
+++ modules/module3.py
@@ -0,0 +1,18 @@
+"""Docstring."""
+
+
+class Class3:
+    def __init__(self) -> None:
+        pass
+
+    def test_function(self, a, b):
+        """_summary_
+
+        Args:
+            a (_type_): _description_
+            b (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        return a * b
---
+++ unit_tests/test_module3.py
@@ -0,0 +1,10 @@
+"""Docstring."""
+
+from modules.module3 import Class3
+
+
+def test_f1():
+    """_summary_
+    """
+    xx = Class3()
+    assert xx.test_function(4, 5) == 20
